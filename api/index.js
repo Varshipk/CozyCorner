430cd239fb95395d0fa1318d364c7c93706d5e1b
Varshipk <varshipkatiyar@gmail.com>
2024-04-24 00:51:06 +0530

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import { log } from "console";
+import mongoose from "mongoose";
 import express from "express";
+import dotenv from 'dotenv'
+dotenv.config();
+
+
 const app =express();
+mongoose
+.connect(process.env.MONGODB)
+.then(()=>{
+    console.log("DB is connected");
+})
+.catch((e)=>{
+    console.log(e);
+})
 app.listen(3000,()=>{
     console.log("server is running on 3000");
 })

182d460452baf9687ff0bcaaee62c696825ad1d3
Varshipk <varshipkatiyar@gmail.com>
2024-04-24 03:45:56 +0530

    create sigup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import User from "../models/user.js";
+import bcryptjs from 'bcryptjs';
+export const signup = async (req,res)=>{
+      const {username,email,password} =req.body;
+      const hashedPassword =bcryptjs.hashSync(password,10);
+     const newUser =new User({username,email,password:hashedPassword});
+     try{
+        await newUser.save()
+        res.status(201).json("user created succesfully");
+     } catch(error){
+        res.status(501).json(error.message)
+     }  
+}
--- api/index.js
+++ api/index.js
@@ -3,10 +3,9 @@ import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from './routes/user.route.js'
-
-
-
+import authRouter from './routes/auth.route.js'
 const app =express();
+app.use(express.json());
 mongoose
 .connect(process.env.MONGODB)
 .then(()=>{
@@ -20,4 +19,4 @@ app.listen(3000,()=>{
 })
 
 app.use('/api/user',userRouter);
-
+app.use('/api/auth',authRouter)

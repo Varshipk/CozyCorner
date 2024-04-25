4e97914bc100e8b67e1e7b90524628ae7b055811
Varshipk <varshipkatiyar@gmail.com>
2024-04-26 01:40:18 +0530

    create sign in api and complete sign in page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 export const signup = async (req,res,next)=>{
       const {username,email,password} =req.body;
       const hashedPassword =bcryptjs.hashSync(password,10);
@@ -11,4 +14,20 @@ export const signup = async (req,res,next)=>{
      } catch(error){
         next(error)
      }  
+}
+
+
+export const signin=async(req,res,next)=>{
+   const {email,password} =req.body;
+    try{
+      const validUser = await User.findOne({email});
+      if(!validUser) return next(errorHandler(404,"User not found"));
+      const validPassword =bcryptjs.compareSync(password,validUser.password);
+      if(!validPassword) return next(errorHandler(401,"Email or Password is incorrect"));
+      const token =jwt.sign({id:validUser._id},process.env.JWTSecret);
+      const {password:pass,...rest}=validUser._doc;
+      res.cookie('accessToken',token,{httpOnly:true}).status(200).json(rest);
+    } catch(error){
+         next(error);
+    }
 }

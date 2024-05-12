d4a289b2be4545376bb3f48f02887dd37a102bc5
Varshipk <varshipkatiyar@gmail.com>
2024-05-12 19:52:59 +0530

    complete delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,34 +1,40 @@
-
 import User from "../models/user.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs'
-
 export const test= (req,res)=>{
     res.send("This is test route");
 }
-
 export const  userUpdate = async (req,res,next)=>{
 if(req.user.id!== req.params.id) return next(errorHandler(401,'You can update your own account'));
 try{
     if(req.body.password){
         req.body.password =bcryptjs.hashSync(req.body.password,10);
     }
-    const updateUser =await User.findByIdAndUpdate(
-       req.params.id, {
+    const updatedUser =await User.findByIdAndUpdate(
+       req.params.id,{
         $set:{
             username:req.body.username,
             email:req.body.email,
             password:req.body.password,
             avatar:req.body.avatar,
         },
-       } ,
+       },
      {new:true}  
     );
-
-    const {password,...rest} =updateUser._doc;
-
+    const {password,...rest} =updatedUser._doc;
     res.status(200).json(rest);
 } catch (error){
     next(error);
 }
+};
+
+export const deleteUser =async(req,res,next)=>{
+    if(req.user.id !==req.params.id) return next(errorHandler(401,"You can delete your account only"));
+    try{
+          await User.findByIdAndDelete(req.params.id);
+          res.clearCookie('access_token');
+          res.status(200).json("User has been deleted");
+    } catch(error){
+        next(error);
+    }
 } 

d9a446538593142ad4a85c0e8853837958a94bd8
Varshipk <varshipkatiyar@gmail.com>
2024-05-18 01:21:36 +0530

    create Listing page and add swiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import AddListing from "./pages/AddListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 
 export default function App() {
@@ -20,6 +21,7 @@ export default function App() {
             <Route path='/about' element ={<About/>} />
             <Route path='/sign-in' element ={<SignIn/>} />
             <Route path='/sign-up' element ={<SignUp/>} />
+            <Route path='/listing/:listingId' element ={<Listing/>} />
             <Route element={<PrivateRoute/>}>
             <Route path='/profile' element ={<Profile/>} />
             <Route path='/create-listing' element={<AddListing/>} />

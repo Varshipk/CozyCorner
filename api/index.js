01d19aededd6d1bd9d9f60b094736b62d241021a
Varshipk <varshipkatiyar@gmail.com>
2024-05-21 02:34:06 +0530

    add build script and path

--- api/index.js
+++ api/index.js
@@ -1,35 +1,44 @@
 import mongoose from "mongoose";
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import listingRouter from './routes/listing.route.js'
-const app =express();
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import path from "path";
+const __dirname = path.resolve();
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
 mongoose
-.connect(process.env.MONGODB)
-.then(()=>{
+  .connect(process.env.MONGODB)
+  .then(() => {
     console.log("DB is connected");
-})
-.catch((e)=>{
+  })
+  .catch((e) => {
     console.log(e);
-})
-app.listen(3000,()=>{
-    console.log("server is running on 3000");
-})
+  });
+app.listen(3000, () => {
+  console.log("server is running on 3000");
+});
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/listing',listingRouter);
-app.use((err,req,res,next)=>{
-   const  statusCode=err.statusCode||500;
-   const   message =err.message||"Internal server error";
-   return res.status(statusCode).json({
-    success:false,
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
     statusCode,
     message,
-   })
-})
+  });
+});
